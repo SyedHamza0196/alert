7a45e91d9a1ae5e2a33f56b81d5d4a4bb6bc609b
SyedHamza0196 <hamzasyed001230@gmail.com>
2021-05-28 12:36:59 +0500

    saving

--- opencv_video_save.py
+++ opencv_video_save.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 from datetime import datetime,date
+import time
 import configparser
 
+start_time = time.time()
 config = configparser.ConfigParser()
 config.read("./config.yaml")
 
 rtsp = config['rtsp']['link']
+recording_ending_time = config['time']['recording_ending_time']
 
 current_time = datetime.now().strftime("%H%M%S")
 current_date = date.today().strftime("%d%m%Y")
@@ -24,12 +27,9 @@ out = cv2.VideoWriter(current_time+'_'+current_date+'.mp4',cv2.VideoWriter_fourc
 while(True):
     ret, frame = cap.read()
     
-    if ret == True:
+    if ret == True and int(time.time()-start_time) == recording_ending_time:
         out.write(frame)
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #     break
     else:
         break
-cap.release()
-out.release()
-cv2.destroyAllWindows()
